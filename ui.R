cb9144dd56204a187cda2d02c19c891d882bc5cc
LMayhew <lmayhew2002@yahoo.com>
2017-06-19 21:31:29 -0600

    Put calculation info on doc page.

--- ui.R
+++ ui.R
@@ -15,7 +15,7 @@ shinyUI(fluidPage(
       ),
       h4("Things to Try!"),
       p("1. Select a region of the graph by clicking and dragging the cursor."),
-      p("2. If a region is selected, check the Show Model checkbox 
+      p("2. If a region is selected, check the Show Model Outputs checkbox 
         to see fit line on graph and display intercept, slope values"),
       p("3. Check the Hurricane Katrina and/or Sandy buttons to see 
         the data points of these two big hurricanes."),
@@ -28,11 +28,25 @@ shinyUI(fluidPage(
               plotOutput("plot1",brush=brushOpts(id="plotBrush"),height="600px",width="600px"),
               textOutput("modelOutputs")
           ),
-          tabPanel("About the Data...",br(),
+          tabPanel("Documentation",br(),
+              h4("About the data..."),
               p('The Names US Atlantic Hurricanes data is from the R package "DAAG" 
                      and gives details of atmosperic pressure and windspeed at landfall,
                      estimated damage in millions of dollars and deaths for named
-                     hurricanes that made landfall in the US mainland from 1950 to 2012.')
+                     hurricanes that made landfall in the US mainland from 1950 to 2012.'),
+              br(),
+              h4("About the calculations..."),
+              p("The linear model used to fit the relationship between windspeed
+                and damage was the 'lm' package.  The formula is"),
+              p("damage = m * windspeed + b"),
+              p("where m is the slope and b is the intercept.  On the plot tab you can
+                see the calculated intercept and slope (and resulting line on the graph)
+                provided you selected a region and also checked the Show Model 
+                Outputs checkbox."),
+              p("In addition, you can limit the amount of data used in the calculation
+                by selected not all the points and also by reducing the range of years
+                selected using the slider.")
+                
           )
        )
     )
