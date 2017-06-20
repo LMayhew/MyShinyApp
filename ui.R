484ba6089ac5b86d55c324bbc01fdc1ab6931bfc
LMayhew <lmayhew2002@yahoo.com>
2017-06-19 20:57:09 -0600

    Documentation Tabs

--- ui.R
+++ ui.R
@@ -6,20 +6,37 @@ shinyUI(fluidPage(
 
   sidebarLayout(
     sidebarPanel(
-      p("Things to try:"),
-      p("1. Select a region of the graph"),
-      p("2. Check Show Model to see fit line on graph and intercept, slope values"),
-      p("3. Uncheck the Hurricane Katrina and/or Sandy buttons"),
-      p("4. Use the slider to select the years for the plot and model"),
-      checkboxInput("showModelOutputs","Show Model Outputs",value=TRUE),
-      checkboxInput("showKatrina","Indicate Hurricane Katrina (Red)",value=TRUE),
-      checkboxInput("showSandy","Indicate Hurricane Sandy (Green)",value=TRUE),
+      checkboxInput("showModelOutputs","Show Model Outputs",value=FALSE),
+      checkboxInput("showKatrina","Indicate Hurricane Katrina (Red)",value=FALSE),
+      checkboxInput("showSandy","Indicate Hurricane Sandy (Green)",value=FALSE),
       sliderInput("yearSlider","Limit the Number of Years",1950,2012,value=c(1950,2012))
     ),
 
     mainPanel(
-      plotOutput("plot1",brush=brushOpts(id="plotBrush"),height="600px",width="600px"),
-      textOutput("modelOutputs")
+       tabsetPanel(type="tabs",
+          tabPanel("Play With Plot!",br(),
+              plotOutput("plot1",brush=brushOpts(id="plotBrush"),height="600px",width="600px"),
+              textOutput("modelOutputs")
+          ),
+          tabPanel("Documentation",br(),
+                   h4("Things to Try!"),
+                   p("1. Select a region of the graph by clicking and dragging the cursor."),
+                   p("2. If a region is selected, check the Show Model checkbox 
+                     to see fit line on graph and display intercept, slope values"),
+                   p("3. Check the Hurricane Katrina and/or Sandy buttons to see which
+                     the windspeeds and damage of these two big hurricanes."),
+                   p("4. Use the slider to select the years to limit the number
+                     of points plotted and used in the model."),
+                   p("5. Hover over a point to see the name of the hurricane."),
+                   br(),
+                   h4("About the Data..."),
+                   p('The Names US Atlantic Hurricanes data is from the R package "DAAG" 
+                     and gives details of atmosperic pressure and windspeed at landfall,
+                     estimated damage in millions of dollars and deaths for named
+                     hurricanes that made landfall in the US mainland from 1950 to 2012.')
+                   
+          )
+       )
     )
   )
 ))
